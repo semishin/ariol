eecf6fbe1ec0ba26b97c499789222a731df8dd05
semishin <sembegw@gmail.com>
2015-01-21 16:24:05 +0300

    категории замененны на пдо

--- application/classes/Controller/Site/Ourproduct.php
+++ application/classes/Controller/Site/Ourproduct.php
@@ -7,20 +7,20 @@ class Controller_Site_Ourproduct extends Controller_Site
     public function action_index()
     {
         $ourproduct = ORM::factory('Ourproduct')
-            ->where('active','=',1)
-            ->order_by('position','asc')
+            ->where('active', '=', 1)
+            ->order_by('position', 'asc')
             ->limit(self::LIMIT_ON_PAGE)
             ->find_all()
             ->as_array();
 
         $ourproduct_category = ORM::factory('Ourproduct_Category')
-            ->where('active','=',1)
-            ->order_by('position','asc')
+            ->where('active', '=', 1)
+            ->order_by('position', 'asc')
             ->find_all()
             ->as_array();
 
         $count_product = ORM::factory('Ourproduct')
-            ->where('active','=',1)
+            ->where('active', '=', 1)
             ->count_all();
 
         $this->template->ourproduct = $ourproduct;
@@ -46,10 +46,10 @@ class Controller_Site_Ourproduct extends Controller_Site
             $this->forward_404();
         }
 
-        $count_product = ORM::factory('Ourproduct')->where('active','=',1);
+        $count_product = ORM::factory('Ourproduct')->where('active', '=', 1);
         if ($category_id) {
             $temp_id = ORM::factory('Prodcat')
-                ->where('cat_id','=',$category_id)
+                ->where('cat_id', '=', $category_id)
                 ->find_all();
 
             $products_ids = array();
@@ -58,9 +58,9 @@ class Controller_Site_Ourproduct extends Controller_Site
             }
 
             if ($products_ids) {
-                $count_product = $count_product->where('id','IN',$products_ids);
+                $count_product = $count_product->where('id', 'IN', $products_ids);
             } else {
-                $count_product = $count_product->where('id','=', 'DISABLED');
+                $count_product = $count_product->where('id', '=', 'DISABLED');
             }
         }
         if ($on_main) {
@@ -69,11 +69,11 @@ class Controller_Site_Ourproduct extends Controller_Site
         $count_product = $count_product->count_all();
 
         $ourproduct = ORM::factory('Ourproduct')
-            ->where('active','=',1);
+            ->where('active', '=', 1);
 
         if ($category_id) {
             $temp_id = ORM::factory('Prodcat')
-                ->where('cat_id','=',$category_id)
+                ->where('cat_id', '=', $category_id)
                 ->find_all();
 
             $products_ids = array();
@@ -82,9 +82,9 @@ class Controller_Site_Ourproduct extends Controller_Site
             }
 
             if ($products_ids) {
-                $ourproduct = $ourproduct->where('id','IN',$products_ids);
+                $ourproduct = $ourproduct->where('id', 'IN', $products_ids);
             } else {
-                $ourproduct = $ourproduct->where('id','=', 'DISABLED');
+                $ourproduct = $ourproduct->where('id', '=', 'DISABLED');
             }
         }
 
@@ -93,21 +93,21 @@ class Controller_Site_Ourproduct extends Controller_Site
         }
 
         $ourproduct = $ourproduct
-            ->order_by('position','asc')
+            ->order_by('position', 'asc')
             ->limit(self::LIMIT_ON_PAGE)
             ->offset($offset)
             ->find_all()
             ->as_array();
 
         exit(
-          json_encode(
-              array(
+        json_encode(
+            array(
                 'html' => View::factory('site/ourproduct/more', array(
                     'ourproduct' => $ourproduct,
                 ))->render(),
                 'more' => $count_product >= self::LIMIT_ON_PAGE * ($offset / self::LIMIT_ON_PAGE) + self::LIMIT_ON_PAGE,
-              )
-          )
+            )
+        )
         );
     }
 
@@ -115,11 +115,11 @@ class Controller_Site_Ourproduct extends Controller_Site
     {
         $category_id = $this->request->post('category_id');
         $count_product = ORM::factory('Ourproduct')
-            ->where('active','=',1);
+            ->where('active', '=', 1);
 
         if ($category_id) {
             $temp_id = ORM::factory('Prodcat')
-                ->where('cat_id','=',$category_id)
+                ->where('cat_id', '=', $category_id)
                 ->find_all();
 
             $products_ids = array();
@@ -127,7 +127,7 @@ class Controller_Site_Ourproduct extends Controller_Site
                 $products_ids[] = $relation->prod_id;
             }
             if ($products_ids) {
-                $count_product = $count_product->where('id','IN',$products_ids);
+                $count_product = $count_product->where('id', 'IN', $products_ids);
             }
 
         }
@@ -135,35 +135,53 @@ class Controller_Site_Ourproduct extends Controller_Site
         $count_product = $count_product->count_all();
 
 
-        if ($category_id && $products_ids) {
-            $ourproduct = ORM::factory('Ourproduct')
-                ->where('active', '=', 1)
-                ->where('id','IN',$products_ids)
-                ->limit(self::LIMIT_ON_PAGE)
-                ->find_all()->as_array();
-        } elseif($category_id) {
-            $ourproduct = ORM::factory('Ourproduct')
-                ->where('active', '=', 1)
-                ->limit(0)
-                ->find_all()->as_array();
-        } else {
-            $ourproduct = ORM::factory('Ourproduct')
-                ->where('active', '=', 1)
-                ->limit(self::LIMIT_ON_PAGE)
-                ->find_all()->as_array();
-        }
+/*        if ($category_id && $products_ids) {
+                        $ourproduct = ORM::factory('Ourproduct')
+                            ->where('active', '=', 1)
+                            ->where('id','IN',$products_ids)
+                            ->limit(self::LIMIT_ON_PAGE)
+                            ->find_all()->as_array();
+                    } elseif($category_id) {
+                        $ourproduct = ORM::factory('Ourproduct')
+                            ->where('active', '=', 1)
+                            ->limit(0)
+                            ->find_all()->as_array();
+                    } else {
+                        $ourproduct = ORM::factory('Ourproduct')
+                            ->where('active', '=', 1)
+                            ->limit(self::LIMIT_ON_PAGE)
+                            ->find_all()->as_array();
+                    }
+*/
+            $PDO = ORM::factory('Ourproduct')->PDO();
 
-        exit(
-            json_encode(
-                array(
-                    'html' => View::factory('site/ourproduct/category', array(
-                        'ourproduct' => $ourproduct,
-                    ))->render(),
-                    'more' => $count_product > self::LIMIT_ON_PAGE
+            $query = "SELECT prod.id as id, prod.image as image,
+                          prod.name as name, prod.url as url
+                  FROM ourproducts prod
+                  LEFT JOIN prod_cat ON prod.id = prod_cat.prod_id
+                  LEFT JOIN ourproducts_categories cat ON cat.id = prod_cat.cat_id
+                  WHERE prod.active = 1";
+            if ($category_id) {
+                $query .= " AND cat.id={$category_id}";
+            }
+            $query .= " GROUP BY prod.id";
+
+            $PDO = $PDO->prepare($query);
+            $PDO->execute();
+            $ourproduct = $PDO->fetchAll(PDO::FETCH_ASSOC);
+
+            exit(
+                json_encode(
+                    array(
+                        'html' => View::factory('site/ourproduct/category', array(
+                            'ourproduct' => $ourproduct,
+                        ))->render(),
+                        'more' => $count_product > self::LIMIT_ON_PAGE
+                    )
                 )
-            )
-        );
+            );
 
 
-    }
+        }
 }
+
--- application/views/site/ourproduct/category.php
+++ application/views/site/ourproduct/category.php
@@ -1,4 +1,4 @@
-<?php foreach ($ourproduct as $item) { ?>
+<?php/* foreach ($ourproduct as $item) { ?>
     <div class="col-md-3 col-sm-6">
         <a href="<?php echo $item->url ?>"><img src="<?php echo Lib_Image::crop($item->image, 'ourproduct',$item->id, 315, 227); ?>" alt="<?php echo $item->name ?>" class="img-responsive"></a>
         <p class="text-center"><a  href="<?php echo $item->url ?>" class="text_link"><?php echo $item->name ?></a></p>
@@ -8,6 +8,20 @@
                 <?php $names[] = mb_ucfirst(mb_strtolower($cat->name)); ?>
             <?php } ?>
             <?php echo join(', ', $names); ?>
+        </p>
+    </div>
+<?php }*/ ?>
+
+<?php foreach ($ourproduct as $item) { ?>
+    <div class="col-md-3 col-sm-6">
+        <a href="<?php echo $item["url"] ?>"><img src="<?php echo Lib_Image::crop($item["image"], 'ourproduct',$item["id"], 315, 227); ?>" alt="<?php echo $item["name"] ?>" class="img-responsive"></a>
+        <p class="text-center"><a  href="<?php echo $item["url"] ?>" class="text_link"><?php echo $item["name"] ?></a></p>
+        <p class="text-center">
+            <?php //$names = array(); ?>
+            <?php //foreach ($item->categories->find_all() as $cat) { ?>
+                <?php //$names[] = mb_ucfirst(mb_strtolower($cat->name)); ?>
+            <?php //} ?>
+            <?php //echo join(', ', $names); ?>
         </p>
     </div>
 <?php } ?>
