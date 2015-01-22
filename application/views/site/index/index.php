ac82c53c62009a677b551fc77af919a357671ef5
semishin <sembegw@gmail.com>
2015-01-22 18:04:41 +0300

    исправленны урлы на работы

--- application/views/site/index/index.php
+++ application/views/site/index/index.php
@@ -37,8 +37,8 @@
         <div class="row">
             <?php foreach ($ourproduct as $item) { ?>
                 <div class="col-md-3 col-sm-6">
-                    <a href="<?php echo $item->url ?>"><img src="<?php echo Lib_Image::crop($item->image, 'ourproduct',$item->id, 315, 227); ?>" alt="<?php echo $item->name ?>" class="img-responsive"></a>
-                    <p class="text-center"><a  href="<?php echo $item->url ?>" class="text_link"><?php echo $item->name ?></a></p>
+                    <a href="/portfolio/<?php echo $item->url ?>"><img src="<?php echo Lib_Image::crop($item->image, 'ourproduct',$item->id, 315, 227); ?>" alt="<?php echo $item->name ?>" class="img-responsive"></a>
+                    <p class="text-center"><a  href="/portfolio/<?php echo $item->url ?>" class="text_link"><?php echo $item->name ?></a></p>
                     <p class="text-center">
                         <?php $names = array(); ?>
                         <?php foreach ($item->categories->find_all() as $cat) { ?>
